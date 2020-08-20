50dee8586c7b916d50428e120b68dcd95fb33fba
danalex032003 <52310324+danalex032003@users.noreply.github.com>
2020-08-20 20:58:32 +0300

    Login and Register

--- app/src/main/java/com/example/getoveritapp/MainActivity.java
+++ app/src/main/java/com/example/getoveritapp/MainActivity.java
@@ -9,19 +9,26 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button goToLoginButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        goToLoginButton = (Button)findViewById(R.id.goToLoginButton);
+        Button goToLoginButton = (Button) findViewById(R.id.goToLoginButton);
+        Button goToRegisterButton = (Button) findViewById(R.id.goToRegisterButton);
 
         goToLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        goToRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/getoveritapp/entities/UserEntity.java
@@ -0,0 +1,79 @@
+package com.example.getoveritapp.entities;
+
+
+import java.util.List;
+
+public class UserEntity {
+    private String email;
+    private String password;
+    private String username;
+    private String name;
+    private Integer typeOfUser;
+    private List<UserEntity> userEntities;
+
+    public UserEntity(String email, String password, String username, String name, Integer typeOfUser, List<UserEntity> userEntities) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.name = name;
+        this.typeOfUser = typeOfUser;
+        this.userEntities = userEntities;
+    }
+
+    public UserEntity(String email, String password, String username, String name) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.name = name;
+    }
+
+    public UserEntity(String email, String password, String username) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+    public UserEntity() {
+
+    }
+
+    public String getEmail(String email) {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword(String password) {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername(String username) {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getTypeOfUser() {
+        return typeOfUser;
+    }
+
+    public void setTypeOfUser(Integer typeOfUser) {
+        this.typeOfUser = typeOfUser;
+    }
+}
