34784f16c1615d1d5f3a797d7e192f55b67fe341
danalex032003 <52310324+danalex032003@users.noreply.github.com>
2021-01-22 09:39:21 +0200

    Changed fragment_home.xml and HomeFragment.java

--- app/src/main/java/com/example/getoveritapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/getoveritapp/ui/home/HomeFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,7 +28,7 @@ public class HomeFragment extends Fragment {
 
     private static final UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static final String ADDRESS = "98:D3:71:F6:11:BE";
-    private static final String STRING_SENT = "92";
+    private static final int INT_SENT = 8;
 
     private HomeViewModel homeViewModel;
 
@@ -35,6 +37,8 @@ public class HomeFragment extends Fragment {
     private OutputStream outputStream;
 
     private ImageView connect;
+    private EditText optionEditText;
+    private Button sendOption;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -44,6 +48,10 @@ public class HomeFragment extends Fragment {
 
 
         connect = root.findViewById(R.id.bluetooth_connect);
+        optionEditText = root.findViewById(R.id.option_edit_text);
+        sendOption = root.findViewById(R.id.nu_inteleg_ce_are);
+
+
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,7 +65,7 @@ public class HomeFragment extends Fragment {
                     bluetoothSocket.connect();
                     System.out.println(bluetoothSocket.isConnected());
                     outputStream = bluetoothSocket.getOutputStream();
-                    write(STRING_SENT);
+                    write(INT_SENT);
 
                 }
                 catch (IOException e) {
@@ -67,12 +75,19 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        sendOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                write(Integer.parseInt(optionEditText.getText().toString()));
+            }
+        });
+
         return root;
     }
 
-    public void write(String s) {
+    public void write(int n) {
         try {
-            bluetoothSocket.getOutputStream().write(s.getBytes());
+            bluetoothSocket.getOutputStream().write(n);
         } catch (IOException e) {
             e.printStackTrace();
         }
